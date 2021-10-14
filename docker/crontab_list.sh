5a24aad99aa9489949a4864df125eb66216f5db5
jinhui <1322630531@qq.com>
2021-10-14 08:29:37 +0800

    10.14 8.29

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -76,6 +76,8 @@
 45 0,12,21 * * * node /scripts/jd_opencard3.js >> /scripts/logs/jd_opencard3.log 2>&1
 #jd_big_winner_Mod
 40 8,10,12,14 * * * node /scripts/jd_big_winner_Mod.js >> /scripts/logs/jd_big_winner_Mod.log 2>&1
+#东东世界兑换
+45 0 0,9 * * * node /scripts/jd_ddworld_exchange.js >> /scripts/logs/jd_ddworld_exchange.log 2>&1
 
 ##############长期活动##############
 # 签到
